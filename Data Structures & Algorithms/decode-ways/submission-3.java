1a699eb9492dbe9984abd27fddca1dd0349e8c4f
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-04-19 19:16:14 +0900

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = (s.charAt(0) == '0') ? 0 : 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            int oneDigit = s.charAt(i - 1) - '0';
+            int twoDigit = (s.charAt(i - 2) - '0') * 10 + oneDigit;
+
+            if (oneDigit != 0) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (10 <= twoDigit && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
