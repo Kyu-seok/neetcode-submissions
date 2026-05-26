6602e20a022e47bea8a57da961c991506029c1bc
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-05-26 15:54:44 +0900

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        int i = 0;
+
+        for (i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor ^= i;
+    }
+}
