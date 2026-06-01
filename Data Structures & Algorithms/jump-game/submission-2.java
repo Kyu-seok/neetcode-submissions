38ed21c53ca2b788cf23b1f79f3c309b736201a0
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-06-01 16:33:21 +0900

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxIndex = nums[0];
+
+        for (int i = 0; i <= maxIndex; i++) {
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+            if (maxIndex >= nums.length - 1) return true;
+        }
+
+        return false;
+    }
+}
