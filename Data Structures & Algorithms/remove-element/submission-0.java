c22079ecb1d8c6cba00f7efd6c97af3d1cb980fb
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-06-24 21:18:59 +0900

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            if (nums[l] == val) {
+                nums[l] = nums[r];
+                // this could be negelected if the value after do not matter
+                nums[r] = val;
+                r--;
+            } else {
+                l++;
+            }
+        }
+
+        return l;
+    }
+}
