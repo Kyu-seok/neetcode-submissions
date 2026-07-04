23bfe5135ed45e9924f6bc0c044be4feacbe9b43
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-07-05 00:47:37 +0900

    Add: design-circular-queue - submission-1

---
+++ Data Structures & Algorithms/design-circular-queue/submission-1.java
@@ -0,0 +1,55 @@
+class MyCircularQueue {
+
+    int[] arr;
+    int size = 0;
+    int frontIndex = 0;
+    int k;
+
+    public MyCircularQueue(int k) {
+        this.k = k;
+        this.arr = new int[k];
+    }
+    
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        arr[(frontIndex + size) % k] = value;
+        size++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        frontIndex = (frontIndex + 1) % k;
+        size--;
+        return true;
+    }
+    
+    public int Front() {
+        if (isEmpty()) return -1;
+        return arr[frontIndex];
+    }
+    
+    public int Rear() {
+        if (isEmpty()) return -1;
+        return arr[(frontIndex + size - 1) % k];
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public boolean isFull() {
+        return size == k;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
