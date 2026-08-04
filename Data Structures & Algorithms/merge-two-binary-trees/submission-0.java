ab4ec6e338346195eac05c7f0c60e5d53c7852c6
Q, the programmer <58456079+Kyu-seok@users.noreply.github.com>
2026-08-04 16:21:42 +0900

    Add: merge-two-binary-trees - submission-0

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-0.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return dfs(root1, root2);
+    }
+
+    private TreeNode dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        TreeNode newRoot = new TreeNode(root1.val + root2.val);
+        newRoot.left = dfs(root1.left, root2.left);
+        newRoot.right = dfs(root1.right, root2.right);
+
+        return newRoot;
+    }
+}
